1760c345a2749d1acee43cf4f8da6c23cf0d2ee5
muhandislikm-tech <muhandislikm@gmail.com>
2026-02-19 13:17:23 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,12 +2,11 @@ import telebot
 import os
 
 TOKEN = os.environ.get("TOKEN")
-GROUP_ID = int(os.environ.get("GROUP_ID"))
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(func=lambda message: True)
-def forward_message(message):
-    bot.forward_message(GROUP_ID, message.chat.id, message.message_id)
+def get_id(message):
+    bot.reply_to(message, f"Chat ID: {message.chat.id}")
 
 bot.polling()
