244923452db16c4ab101f4094c79f9f8767298a7
muhandislikm-tech <muhandislikm@gmail.com>
2026-02-23 11:07:08 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,12 +6,23 @@ GROUP_ID = int(os.getenv("GROUP_ID"))
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(func=lambda message: True)
+# 1) User yozsa → guruhga forward
+@bot.message_handler(func=lambda message: message.chat.type == "private")
 def forward_to_group(message):
     bot.forward_message(
         GROUP_ID,
         message.chat.id,
         message.message_id
     )
+
+# 2) Guruhda reply qilinsa → userga qaytar
+@bot.message_handler(func=lambda message: message.chat.id == GROUP_ID and message.reply_to_message)
+def reply_to_user(message):
+    original = message.reply_to_message
+
+    # Forward qilingan xabarda forward_from bo‘ladi
+    if original.forward_from:
+        user_id = original.forward_from.id
+        bot.send_message(user_id, message.text)
 
 bot.infinity_polling()
